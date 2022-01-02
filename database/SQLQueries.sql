5a2778d8671d25f037a7c67fd5c420772977c266
Kai <77874716+KaiW96@users.noreply.github.com>
2022-01-01 19:08:26 -0700

    Update SQLQueries.sql

--- database/SQLQueries.sql
+++ database/SQLQueries.sql
@@ -114,34 +114,34 @@ SELECT DISTINCT document.id, file, TO_BASE64(file),
 --used for testing purposes
 DELETE FROM document where document.id = 2;
 
-##Select all teachers evaluation: outputs, firstname, lastname, teacherid and Questions
+--Select all teachers evaluation: outputs, firstname, lastname, teacherid and Questions
 SELECT firstname, lastname, teacher.teacherid, Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9,Q10
 	FROM teacher, user, teacher_evaluation
 		WHERE teacher.username = user.username AND 
 			  teacher.teacherid = teacher_evaluation.teacherid;
 
-##Select the teacher evaluation using teacherid, output firstname and lastname included
+--Select the teacher evaluation using teacherid, output firstname and lastname included
 SELECT firstname, lastname, teacher.teacherid, Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9,Q10
 	FROM teacher, user, teacher_evaluation
 		WHERE teacher.username = user.username AND 
 			  teacher.teacherid = teacher_evaluation.teacherid AND 
 			  teacher.teacherid = 10001;
 			  
-##Select the evaluations for TAs only, outputs firstname, lastname and questions
+--Select the evaluations for TAs only, outputs firstname, lastname and questions
 SELECT firstname, lastname, teacher.teacherid, Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9,Q10
 	FROM teacher, user, teacher_evaluation
 		WHERE teacher.username = user.username AND 
 			  teacher.teacherid = teacher_evaluation.teacherid AND 
 			  teacher.isTA = "TA";
 
-##Select students class List of a course
+--Select students class List of a course
 SELECT DISTINCT user.firstname, user.lastname, email
 	FROM course, user, student, takes
 		WHERE (student.studentID = takes.studentID AND
 		user.role = 'student'
 		AND takes.courseID = 471);
 
-##Select teachers class List of a course
+--Select teachers class List of a course
 SELECT user.firstname, user.lastname, email
 	FROM user, course, courseteacher, teacher
 		WHERE ( 
@@ -150,15 +150,15 @@ SELECT user.firstname, user.lastname, email
 	    AND courseteacher.courseid = 471
 	);
 
-##Delete statements, will cascade into other tables.
+--Delete statements, will cascade into other tables.
 DELETE FROM student WHERE student.username = "JayStudent"; 
 DELETE FROM user WHERE username = "Ali";
 
-##Delete a teacher. The teacher will be deleted from teacher_evaluation, teacher, courseteacher. course will not be affected
+--Delete a teacher. The teacher will be deleted from teacher_evaluation, teacher, courseteacher. course will not be affected
 DELETE FROM user WHERE user.username = "Pafederl";
 
 
-
+--Questions used for the student-teacher evaluations form
 ##Q1 The TA starts the lab session on time:
 
 ##Q2 The TA uses the time of the lab effectively:
@@ -181,63 +181,16 @@ DELETE FROM user WHERE user.username = "Pafederl";
 
 
 
-INSERT INTO Assignment(assignment_id, assignment_name, due_date, content, courseid) VALUES (1,"Homework 1", "2021-12-14", "Solve Question 1", 471);
-INSERT INTO Assignment(assignment_id, assignment_name, due_date, content, courseid) VALUES (2,"Homework 2", "2021-12-14", "Solve Question 2", 471);
-INSERT INTO Assignment(assignment_id, assignment_name, due_date, content, courseid) VALUES (4,"Homework 3", "2021-12-14", "Solve Question 3", 471);
-
-INSERT INTO Assignment(assignment_id, assignment_name, due_date, content, courseid) VALUES (4,"Assignment 1", "2021-12-06", "Solve Assignment 1", 480);
-INSERT INTO Assignment(assignment_id, assignment_name, due_date, content, courseid) VALUES (5,"Assignment 2", "2021-12-06", "Solve Assignment 2", 480);
-INSERT INTO Assignment(assignment_id, assignment_name, due_date, content, courseid) VALUES (6,"Assignment 3", "2021-12-06", "Solve Assignment 3", 480);
-
-INSERT INTO Assignment(assignment_id, assignment_name, due_date, content, courseid) VALUES (7,"Project 1", "2021-12-08", "Complete Project 1", 511);
-INSERT INTO Assignment(assignment_id, assignment_name, due_date, content, courseid) VALUES (8,"Project 2", "2021-12-10", "Complete Project 2", 511);
-
-INSERT INTO Assignment(assignment_id, assignment_name, due_date, content, courseid) VALUES (9,"Coding Challenge 1", "2021-11-30", "Complete coding challenge 1", 457);
-INSERT INTO Assignment(assignment_id, assignment_name, due_date, content, courseid) VALUES (10,"Coding Challenge 2", "2021-11-30", "Complete coding challenge 2", 457);
-
-## Select all the assignmnets in a course
+--Select all the assignments in a course
 SELECT Assignment.assignment_name, course.courseid
 	FROM  Assignment, course
 	WHERE course.courseid = Assignment.courseid AND
 		  course.courseid = 471;
 
-## Delete a assignment for a course
+--Delete a assignment for a course
 DELETE FROM Assignment WHERE Assignment.assignment_id = 2; 
 
-
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 1, 1000, 90);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 2, 1000, 90);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 3, 1000, 75);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 4, 1000, 90);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 5, 1000, 80);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 6, 1000, 70);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 7, 1000, 50);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 8, 1000, 100);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 9, 1000, 90);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 10, 1000, 100);
-
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 1, 1001, 70);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 2, 1001, 90);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 3, 1001, 98);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 4, 1001, 40);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 5, 1001, 99);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 6, 1001, 70);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 7, 1001, 60);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 8, 1001, 80);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 9, 1001, 70);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 10, 1001, 95);
-
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 1, 1002, 100);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 2, 1002, 100);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 3, 1002, 100);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 4, 1002, 90);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 5, 1002, 99);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 6, 1002, 70);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 7, 1002, 40);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 8, 1002, 30);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 9, 1002, 0);
-INSERT INTO submit(assignment_id,studentID, grade) VALUES ( 10, 1002, 100);
-
+--Select all assignments for a specific name
 SELECT user.firstname, user.lastname, user.username, student.studentid, submit.assignment_id, Assignment.assignment_name, submit.grade, course.courseid
 	FROM student, user, submit, Assignment, course
 	WHERE student.studentID = submit.studentID AND 
@@ -245,7 +198,8 @@ SELECT user.firstname, user.lastname, user.username, student.studentid, submit.a
 		student.username = user.username AND
 		course.courseid = Assignment.courseid AND
 		user.firstname = "Ali";
-
+		
+--Select all assignments for a specific student in a specific course
 SELECT user.firstname, user.lastname, user.username, student.studentid, submit.assignment_id, Assignment.assignment_name, submit.grade, course.courseid
 	FROM student, user, submit, Assignment, course
 	WHERE student.studentID = submit.studentID AND 
@@ -255,6 +209,7 @@ SELECT user.firstname, user.lastname, user.username, student.studentid, submit.a
 		course.courseid = 471 AND
 		user.username = "AliStudent";
 		
+--Select all assignments for a specific name in a specific course
 SELECT user.firstname, user.lastname, user.username, student.studentid, course.courseid, course.name, AVG(submit.grade)
 	FROM student, user, submit, Assignment, course
 	WHERE student.studentID = submit.studentID AND 
@@ -262,7 +217,7 @@ SELECT user.firstname, user.lastname, user.username, student.studentid, course.c
 		student.username = user.username AND
 		user.firstname = "Ali" AND Assignment.courseid = 457;
 
-
+--Select all assignments for a specific course
 SELECT student.studentid, course.courseid, course.name, submit.grade, Assignment.assignment_id
 	FROM student, submit, Assignment, course
 	WHERE student.studentID = submit.studentID AND 
@@ -270,60 +225,3 @@ SELECT student.studentid, course.courseid, course.name, submit.grade, Assignment
 		Assignment.courseid = course.courseid 
 		and course.courseid = 471;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-INSERT INTO notification(notificationid, date, time, status, courseid) VALUES (01, "2021-11-14", "11:11:11", "Database project is due soon", 471);
-INSERT INTO notification(notificationid, date, time, status, courseid) VALUES (02, "2021-11-14", "11:11:12", "480 project starts next week!", 480);
-
-INSERT INTO receives(notificationid, studentid) VALUES (01,1000); //Gonna need a for loop to send it to all students by changing studentID
-INSERT INTO receives(notificationid, studentid) VALUES (01,1001);
-INSERT INTO receives(notificationid, studentid) VALUES (02,1000);
-INSERT INTO receives(notificationid, studentid) VALUES (02,1001);
-
-SELECT DISTINCT user.firstname, user.lastname, user.username, student.studentid, notification.status, notification.date,notification.time
-	FROM course, student, user, notification, receives
-	WHERE receives.studentID = student.studentID AND 
-		student.username = user.username AND
-		user.firstname = "Jay";
-
-INSERT INTO communication(communication_id, date) VALUES (01, "2021-11-14");
-INSERT INTO communication(communication_id, date) VALUES (02, "2021-11-14");
-
-INSERT INTO discussion(username, name, content,communication_id) VALUES ("JayStudent", "Jay", "I just completed 471 API, project looks great!", 01);
-INSERT INTO discussion(username, name, content, communication_id) VALUES("AliStudent", "Ali", "Good job Jay, I now want you to build an app!", 01);
-
-INSERT INTO email_list(email, studentID, communication_id) VALUES ("JayGurjar@gmail.com", 1000, 01);
-INSERT INTO email_list(email, studentID, communication_id) VALUES ("AliSiddiqi@gmail.com", 1000, 01);
-
-INSERT INTO writes(communication_id, studentID) VALUES (01, 1000);
-INSERT INTO writes(communication_id, studentID) VALUES (02, 1000);
-
-SELECT DISTINCT user.firstname, user.lastname, communication.communication_id, discussion.content, email_list.email
-	FROM user, student, writes, communication, discussion, email_list
-	WHERE user.username = student.username AND
-		student.studentID = writes.studentID AND 
-		writes.communication_id = communication.communication_id AND
-		communication.communication_id = email_list.communication_id AND
-		communication.communication_id = discussion.communication_id;
